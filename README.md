4352ac09d350ac85dc85f47f45aa2ce3d7ff1d74
Nandeesh N <34594610+focusedmonk@users.noreply.github.com>
2023-06-02 01:36:05 +0530

    Moving train and test methods to models.py module

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
+# Description
+This repository contains deep learning model architectures built using pytorch.
+
+---
+
 # Usage
+
+The code snippet below imports required classes and methods.
+
+```
+from utils import device, build_loader
+from model import Net
+```
+
+The code snippet below creates a train and test data loader.
+```
+train_data = # your training dataset
+test_data = # your training dataset
+batch_size = 512
+train_loader, test_loader = build_loader(batch_size, train_data, test_data)
+```
+
+The code snippet below creates a model. `model_train` trains the model on train_loader and `model_test` tests the model on the test_loader.
+```
+model = Net().to(device)
+model.model_train(train_loader, optimizer, criterion)
+model.model_test(test_loader, criterion)
+```
+
+To visualize the model's performance, execute the below script.
+```
+fig, axs = plt.subplots(2,2,figsize=(15,10))
+axs[0, 0].plot(model.train_losses)
+axs[0, 0].set_title("Training Loss")
+axs[1, 0].plot(model.train_acc)
+axs[1, 0].set_title("Training Accuracy")
+axs[0, 1].plot(model.test_losses)
+axs[0, 1].set_title("Test Loss")
+axs[1, 1].plot(model.test_acc)
+axs[1, 1].set_title("Test Accuracy")
+```
--- model.py
+++ model.py
@@ -1,11 +1,20 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from tqdm import tqdm
 
 class Net(nn.Module):
     #This defines the structure of the NN.
     def __init__(self, **kwargs):
         super(Net, self).__init__()
         bias = kwargs.get('bias', True)
+        self.use_device = kwargs.get('device', 'cpu')
+        self.train_acc = []
+        self.train_losses = []
+        self.test_acc = []
+        self.test_losses = []
+
+        # Model architecture
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3, bias=bias)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, bias=bias)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, bias=bias)
@@ -22,3 +31,70 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
+    
+    def get_correct_pred_cnt(self, pPrediction, pLabels):
+        return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
+
+    def model_train(self, train_loader, optimizer, criterion):
+        self.train()
+        pbar = tqdm(train_loader)
+
+        train_loss = 0
+        correct = 0
+        processed = 0
+
+        for batch_idx, (data, target) in enumerate(pbar):
+            data, target = data.to(self.use_device), target.to(self.use_device)
+            optimizer.zero_grad()
+
+            # Predict
+            pred = self(data)
+
+            # Calculate loss
+            loss = criterion(pred, target)
+            train_loss += loss.item()
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+
+            correct += self.get_correct_pred_cnt(pred, target)
+            processed += len(data)
+
+            pbar.set_description(
+                desc=f"Train: Loss={loss.item():0.4f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}"
+            )
+
+        self.train_acc.append(100 * correct / processed)
+        self.train_losses.append(train_loss / len(train_loader))
+        return self.train_acc[-1], self.train_losses[-1]
+
+    def model_test(self, test_loader, criterion):
+        self.eval()
+        test_loss = 0
+        correct = 0
+
+        with torch.no_grad():
+            for batch_idx, (data, target) in enumerate(test_loader):
+                data, target = data.to(self.use_device), target.to(self.use_device)
+
+                output = self(data)
+                test_loss += criterion(
+                    output, target, reduction="sum"
+                ).item()  # sum up batch loss
+
+                correct += self.get_correct_pred_cnt(output, target)
+
+        test_loss /= len(test_loader.dataset)
+
+        print(
+            "Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n".format(
+                test_loss,
+                correct,
+                len(test_loader.dataset),
+                100.0 * correct / len(test_loader.dataset),
+            )
+        )
+        self.test_acc.append(100.0 * correct / len(test_loader.dataset))
+        self.test_losses.append(test_loss)
+        return self.test_acc[-1], self.test_losses[-1]
